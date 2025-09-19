2fcbea62dbebc59f8832cf67d78987af94097d76
brenin35 <brenoca35@gmail.com>
2025-09-19 19:52:01 -0300

    sidebar component

--- codigo/src/routes/+layout.svelte
+++ codigo/src/routes/+layout.svelte
@@ -2,14 +2,20 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster, toast } from 'svelte-sonner';
+	import Sidebar from '$lib/client/components/Sidebar.svelte';
 
 	let { children } = $props();
+
+	let isSidebarOpen = $state(false);
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-
 <Toaster richColors />
-{@render children?.()}
+<Sidebar bind:isOpen={isSidebarOpen} />
+
+<div class="transition-all duration-300 md:ml-[280px]">
+	{@render children?.()}
+</div>
