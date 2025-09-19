4b496aafe1473c4786cdf75022d007f49975c7ac
brenin35 <brenoca35@gmail.com>
2025-09-19 19:47:57 -0300

    página inicial e fix tipo session

--- codigo/src/routes/+page.svelte
+++ codigo/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<div class="flex h-screen items-center justify-center ">
+	<div class=" rounded-lg p-8 text-center">
+		<h1 class="mb-3 text-4xl font-light">Bem vindo</h1>
+		<p class="mb-4 text-5xl font-bold text-primary">{data.session.user.name}</p>
+	</div>
+</div>
