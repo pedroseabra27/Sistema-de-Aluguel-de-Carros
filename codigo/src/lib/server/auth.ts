632fb639d54371cbdf4885e9e23556ccc6db0206
brenin35 <brenoca35@gmail.com>
2025-09-26 20:14:43 -0300

    feat: cargo usuario

--- codigo/src/lib/server/auth.ts
+++ codigo/src/lib/server/auth.ts
@@ -1,14 +1,16 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './db';
-import * as schema from "./db/schema";
+import * as schema from './db/schema';
+import { admin } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg',
-        schema: { ...schema, users: schema.user },
+		schema: { ...schema, users: schema.user }
 	}),
+	plugins: [admin()],
 	emailAndPassword: {
 		enabled: true
-	},
+	}
 });
