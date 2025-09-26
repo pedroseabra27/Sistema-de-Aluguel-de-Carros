9cd55b6cd93a87e92acf3282182367eaddb6218b
brenin35 <brenoca35@gmail.com>
2025-09-26 20:38:32 -0300

    feat: cliente id no pedido

--- codigo/src/lib/server/db/pedido/schema.ts
+++ codigo/src/lib/server/db/pedido/schema.ts
@@ -1,11 +1,13 @@
-import { pgEnum, pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { clienteT } from '../schema';
 
 export const statusPedidoEnum = pgEnum('statusPedido', ['PENDENTE', 'CONCLUIDO', 'CONFIRMADO']);
 
 export const pedidoT = pgTable('pedido', {
 	id: serial('id').primaryKey(),
 	data_pedido: timestamp('data').notNull(),
-	status: statusPedidoEnum()
+	status: statusPedidoEnum(),
+	cliente_id: text('cliente_id').references(() => clienteT.id)
 });
 
 export type SelectPedido = typeof pedidoT.$inferSelect;
