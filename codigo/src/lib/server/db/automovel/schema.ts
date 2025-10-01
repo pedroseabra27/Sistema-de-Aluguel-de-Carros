54eb60e642ee91c50581183a6ce0cf93d39d7c20
brenin35 <brenoca35@gmail.com>
2025-10-01 15:46:16 -0300

    feat: tabela de automovel

---
+++ codigo/src/lib/server/db/automovel/schema.ts
@@ -0,0 +1,16 @@
+import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { user } from '../auth-schema';
+
+export const automovelT = pgTable('automovel', {
+	matricula: serial('matricula').primaryKey(),
+	ano: text('ano').notNull(),
+	modelo: text('modelo').notNull(),
+	marca: text('marca').notNull(),
+	placa: text('placa').notNull(),
+	user_id: text('user_id')
+		.references(() => user.id)
+		.notNull()
+});
+
+export type SelectAutomovel = typeof automovelT.$inferSelect;
+export type InsertAutomovel = typeof automovelT.$inferInsert;
--- codigo/src/lib/server/db/schema.ts
+++ codigo/src/lib/server/db/schema.ts
@@ -1,3 +1,4 @@
 export * from './auth-schema';
 export * from './cliente/schema'
 export * from './pedido/schema'
+export * from './automovel/schema'
