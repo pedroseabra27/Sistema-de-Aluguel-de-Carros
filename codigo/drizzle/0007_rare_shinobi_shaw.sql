8cb2bc0038cfb24c6054e12035a29d7fa38831e0
brenin35 <brenoca35@gmail.com>
2025-10-01 21:29:30 -0300

    feat: novas colunas em pedido

---
+++ codigo/drizzle/0007_rare_shinobi_shaw.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "pedido" ADD COLUMN "data_inicio" timestamp;--> statement-breakpoint
+ALTER TABLE "pedido" ADD COLUMN "data_fim" timestamp;--> statement-breakpoint
+ALTER TABLE "pedido" ADD COLUMN "observacoes" text;
--- codigo/src/lib/server/db/pedido/schema.ts
+++ codigo/src/lib/server/db/pedido/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgEnum, pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 import { clienteT } from '../schema';
 
 export const statusPedidoEnum = pgEnum('statusPedido', ['PENDENTE', 'CONCLUIDO', 'CONFIRMADO']);
@@ -6,6 +6,9 @@ export const statusPedidoEnum = pgEnum('statusPedido', ['PENDENTE', 'CONCLUIDO',
 export const pedidoT = pgTable('pedido', {
 	id: serial('id').primaryKey(),
 	data_pedido: timestamp('data').notNull(),
+	dataInicio: timestamp('data_inicio'),
+	dataFim: timestamp('data_fim'),
+	observacoes: text('observacoes'),
 	status: statusPedidoEnum(),
 	cliente_id: integer('cliente_id').references(() => clienteT.id)
 });
