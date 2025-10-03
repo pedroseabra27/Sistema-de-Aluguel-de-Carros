865f180e0a9fb3550d9810f6ba67ba735fdaa44b
brenin35 <brenoca35@gmail.com>
2025-10-03 18:29:19 -0300

    fix?

--- codigo/src/lib/client/auth-client.ts
+++ codigo/src/lib/client/auth-client.ts
@@ -2,7 +2,6 @@ import { adminClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:5173",
     plugins: [
         adminClient()
     ]
